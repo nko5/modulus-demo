6f231f8bda5b20188debee7941baef1cbbc8b8b2
Jacques Crocker <jacques@nodeknockout.com>
2015-11-06 08:04:09 +0900

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,1 +1,21 @@
-console.log("Post install logic...")
+var http = require('http'),
+    qs = require('querystring'),
+    os = require('os');
+
+var params = {
+  teamcode: "${code}",
+},
+
+options = {
+  host: 'nodeknockout.com',
+  port: 80,
+  path: '/deploys?' + qs.stringify(params)
+};
+
+http.get(options)
+  .on('response', function (res) {
+    if (callback) callback(null, res); 
+ })
+  .on('error', function (err) { 
+    if (callback) callback(err); 
+ })
